f665795763d76d49d87561789845660bc36278c5
ninojaquino <ninojaquino@gmail.com>
2020-03-02 17:45:20 -0800

    pushing lab 4 code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ArtistComponent } from './artist/artist.component';
+import { ArtworkComponent } from './artwork/artwork.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { routerModule } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ArtistComponent,
+    ArtworkComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,38 @@
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ArtistComponent } from './artist/artist.component';
+import { ArtworkComponent } from './artwork/artwork.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        data: {
+            name: 'Home'
+        }
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+        data: {
+            name: 'About'
+        }
+    },
+    {
+        path: 'artist',
+        component: ArtistComponent,
+        data: {
+            name: 'Artist'
+        }
+    },
+    {
+        path: 'artworks',
+        component: ArtworkComponent,
+        data: {
+            name: 'Art Works'
+        }
+    }
+];
+
+export const routerModule = RouterModule.forRoot(routes);
